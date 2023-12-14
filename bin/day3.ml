17a6010557a2c1faf4e9b4b0121789b1c58c9b4a
Zion Koyl <hiszd1@gmail.com>
2023-12-14 12:12:32 -0500

    Working on day 3

--- bin/day3.ml
+++ bin/day3.ml
@@ -1,85 +1,111 @@
-open Core
-
 let input = Advent.read_lines "./inputs/03-test.txt"
-let () = List.iter input ~f:print_endline
-let get_nums (s : string) = Advent.get_all_matches (Str.regexp "[0-9]+") s
+let () = List.iter print_endline input
+let get_nums (s : string) = Advent.get_all_matches (Str.regexp "[0-9]+") s 0
 
 let get_syms (s : string) =
-  Advent.get_all_matches (Str.regexp "[\\_<\\$#\\+*?\\_>]+") s
+  Advent.get_all_matches (Str.regexp "[\\_<\\$#\\+*?\\_>]+") s 0
 ;;
 
-let nums = List.map input ~f:(fun line -> get_nums line)
-let syms = List.map input ~f:(fun line -> get_syms line)
+let all_nums = List.map (fun line -> get_nums line) input
+let all_syms = List.map (fun line -> get_syms line) input
 
 let _ =
-  List.iteri syms ~f:(fun i x ->
-    printf " %s" (Int.to_string i ^ ": ");
-    List.iteri x ~f:(fun j (st, loc, len) ->
-      printf
-        " %s"
-        (Int.to_string j
-        ^ ": ("
-        ^ st
-        ^ ", "
-        ^ Int.to_string loc
-        ^ ", "
-        ^ Int.to_string len
-        ^ ")"));
-    printf "\n")
+  List.iteri
+    (fun i x ->
+      Core.printf " %s" (Int.to_string i ^ ": ");
+      List.iteri
+        (fun j (st, loc, len) ->
+          Core.printf
+            " %s"
+            (Int.to_string j
+            ^ ": ("
+            ^ st
+            ^ ", "
+            ^ Int.to_string loc
+            ^ ", "
+            ^ Int.to_string len
+            ^ ")"))
+        x;
+      Core.printf "\n")
+    all_syms
 ;;
+
+(* let () = *)
+(*   (* let total = List.fold input ~init:0 ~f:(fun acc line -> *) *)
+(*   let nlen = *)
+(*     List.fold all_nums ~init:0 ~f:(fun acc num -> *)
+(*       let _ = printf " %d " acc in *)
+(*       let _ = List.iter num ~f:(fun (nmst, _, _) -> printf "%s " nmst) in *)
+(*       if List.length num = 0 then acc else acc + 1) *)
+(*   in *)
+(*   let _ = printf "\n" in *)
+(*   let slen = *)
+(*     List.fold all_syms ~init:1 ~f:(fun acc sym -> *)
+(*       let _ = printf " a: %d " acc in *)
+(*       let _ = List.iter sym ~f:(fun (nmst, _, _) -> printf "%s " nmst) in *)
+(*       if List.length sym = 0 then acc else acc + 1) *)
+(*   in *)
+(*   let _ = printf "\n" in *)
+(*   print_endline ("nums: " ^ Int.to_string nlen); *)
+(*   print_endline ("syms: " ^ Int.to_string slen); *)
+(*   () *)
+(* ;; *)
 
 let () =
   (* let total = List.fold input ~init:0 ~f:(fun acc line -> *)
-  let _ =
-    List.fold nums ~init:0 ~f:(fun acc num ->
-      let _ =
-        List.fold num ~init:0 ~f:(fun acc (nmst, nmloc, nmlen) ->
-          (* get the numbers from the previous line, current line, and next line *)
-          let syms =
-            match acc with
-            | 0 -> List.nth_exn syms acc @ List.nth_exn syms (acc + 1)
-            | x when x >= List.length input - 1 ->
-              List.nth_exn syms acc @ List.nth_exn syms (acc - 1)
-            | _ ->
-              List.nth_exn syms (acc - 1)
-              @ List.nth_exn syms acc
-              @ List.nth_exn syms (acc + 1)
-          in
-          let _ =
-            printf
-              "%s"
-              ("line: "
-              ^ Int.to_string acc
-              ^ " len: "
-              ^ Int.to_string (List.length syms)
-              ^ " (")
-          in
-          let _ =
-            List.iteri syms ~f:(fun i x ->
-              let st, _, _ = x in
-              let len = List.length syms in
-              if i = len then printf "%s " st else printf "%s, " st)
-          in
-          let nums =
-            List.fold syms ~init:[] ~f:(fun acc (smst, smloc, _) ->
+  let valid_num_list =
+    Core.List.foldi all_nums ~init:[] ~f:(fun ln acc num ->
+      (* num is the list of numbers for this line *)
+      (* create list of posible symbol matches *)
+      let syms =
+        if ln = 0
+        then List.nth all_syms ln @ List.nth all_syms (ln + 1)
+        else if ln = List.length all_syms - 1
+        then List.nth all_syms (ln - 1) @ List.nth all_syms ln
+        else
+          List.nth all_syms (ln - 1)
+          @ List.nth all_syms ln
+          @ List.nth all_syms (ln - 1)
+      in
+      let valid_nums =
+        Core.List.fold num ~init:[] ~f:(fun acc x ->
+          (* this is where we check the numbers to see proximity to symbols *)
+          let st, loc, len = x in
+          let mtch =
+            Core.List.fold syms ~init:false ~f:(fun acc (_, sloc, _) ->
+              let minloc = loc - 1 in
+              let maxloc = loc + len + 1 in
               let _ =
                 print_endline
-                  (smst
+                  (st
+                  ^ " min: "
+                  ^ Int.to_string minloc
+                  ^ " max: "
+                  ^ Int.to_string maxloc
+                  ^ " sloc: "
+                  ^ Int.to_string sloc
                   ^ " loc: "
-                  ^ Int.to_string smloc
-                  ^ " bot: "
-                  ^ Int.to_string (nmloc - 1)
-                  ^ " top: "
-                  ^ Int.to_string (nmloc + 1 + nmlen))
+                  ^ Int.to_string loc)
               in
-              if smloc > nmloc - 1 && smloc < nmloc + 1 + nmlen
-              then [ nmst ] @ acc
-              else acc)
+              if sloc >= minloc && sloc <= maxloc then true else acc)
           in
-          let _ = List.iter nums ~f:(fun x -> print_endline x) in
-          acc + 1)
+          if mtch then [ Core.Int.of_string st ] :: acc else acc)
       in
-      acc + 1)
+      (ln, valid_nums) :: acc)
   in
-  ()
+  Core.List.iter valid_num_list ~f:(fun (ln, nums) ->
+    if List.length nums <> 0
+    then (
+      let _ = Core.printf "%s " ("line: " ^ Int.to_string ln) in
+      let _ =
+        Core.printf
+          " %s "
+          (Core.List.fold nums ~init:"" ~f:(fun acc x ->
+             if acc = ""
+             then Core.List.to_string ~f:Int.to_string x
+             else acc ^ ", " ^ Core.List.to_string ~f:Int.to_string x))
+      in
+      let _ = Core.printf "\n" in
+      ())
+    else ())
 ;;
--- lib/advent.ml
+++ lib/advent.ml
@@ -8,20 +8,23 @@ let read_lines file =
 
 let rec find_all_matches (i : int) (regexp : Str.regexp) (s : string) =
   match Str.search_forward regexp s i with
-  | i -> i :: find_all_matches (i + 1) regexp s
+  | i ->
+    let m = Str.matched_string s in
+    let len = String.length m in
+    i :: find_all_matches (i + len) regexp s
   | exception _ -> []
 ;;
 
-let rec get_all_matches (regexp : Str.regexp) (s : string) =
+let rec get_all_matches (regexp : Str.regexp) (s : string) (start : int) =
   let i =
-    try Str.search_forward regexp s 0 with
+    try Str.search_forward regexp s start with
     | Stdlib.Not_found -> -1
     | Not_found_s _ -> -1
   in
   match i with
   | -1 -> []
-  | x ->
+  | x when x > -1 ->
     let m = Str.matched_string s in
-    (m, x, String.length m)
-    :: get_all_matches regexp (Str.string_after s (x + 1 + String.length m))
+    (m, x, String.length m) :: get_all_matches regexp s x
+  | _ -> []
 ;;
